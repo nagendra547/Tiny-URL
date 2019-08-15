f560543e14e107e8547ea890356745d75ab78689
nagendra547 <nagendra547@yahoo.co.in>
2019-08-15 18:44:48 +0900

    Pom file

---
+++ tinyurl/src/main/java/org/coding/tiny/App.java
@@ -0,0 +1,13 @@
+package org.coding.tiny;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
